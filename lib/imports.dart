ae4ab7bab2001cab10eb8298d8cf7547cf7c166f
AaronUni <Aaron.pohlert@stud.th-luebeck.de>
2021-05-27 12:50:05 +0200

    pos hinzugefügt

--- lib/imports.dart
+++ lib/imports.dart
@@ -2,10 +2,12 @@ library ImmunityTD;
 
 import 'dart:html';
 import 'dart:async';
-import 'dart:math';
+import 'dart:math' as Math;
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 part 'src/models/level.dart';
+part 'src/models/feinde.dart';
+part 'src/models/position.dart';
 part 'src/controller/controller.dart';
 part 'src/view/view.dart';
